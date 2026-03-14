12606b4d41a617a02dc161c34a68011949f6351c
jizogames <elite@jizogames.com>
2026-03-14 19:20:00 +0900

    Update game

--- game/intro.go
+++ game/intro.go
@@ -1,3 +1,17 @@
 package game
 
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Task struct {
+	frame     int
+	Triggered bool
+	Action    func(i *Intro)
+}
+
 type Intro struct{}
+
+func (i *Intro) Update() error {
+	return nil
+}
+
+func (i *Intro) Draw(screen *ebiten.Image) {}
