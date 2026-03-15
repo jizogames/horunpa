55d19788eddd5873f88a87df47f9f9894d5aec0e
jizogames <elite@jizogames.com>
2026-03-15 16:33:52 +0900

    Update game

--- game/intro.go
+++ game/intro.go
@@ -2,16 +2,50 @@ package game
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+type Intro struct {
+	frame int
+
+	tasks   []Task
+	sprites []Sprite
+}
+
+func (i *Intro) Update() {
+	i.frame++
+
+	for idx := range i.tasks {
+		task := &i.tasks[idx]
+		if !task.Triggered && i.frame >= task.Frame {
+			task.Action(i)
+			task.Triggered = true
+		}
+	}
+
+	dst := i.sprites[:0]
+	for _, s := range i.sprites {
+		s.Update()
+		if s.Alive() {
+			dst = append(dst, s)
+		}
+	}
+	i.sprites = dst
+}
+
+func (i *Intro) Draw(screen *ebiten.Image) {
+	for _, s := range i.sprites {
+		s.Draw(screen)
+	}
+}
+
+// タスク.
 type Task struct {
-	frame     int
+	Frame     int
 	Triggered bool
 	Action    func(i *Intro)
 }
 
-type Intro struct{}
-
-func (i *Intro) Update() error {
-	return nil
+// スプライト.
+type Sprite interface {
+	Update()
+	Draw(screen *ebiten.Image)
+	Alive() bool
 }
-
-func (i *Intro) Draw(screen *ebiten.Image) {}
